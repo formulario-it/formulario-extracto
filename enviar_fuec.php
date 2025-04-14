b502a1c5f3b440319f0b05ffdd8bd697caf1e671
formulario-it <extractofuec@gmail.com>
2025-04-14 01:17:40 -0500

    Add files via upload

--- enviar_fuec.php
+++ enviar_fuec.php
@@ -13,7 +13,6 @@ require 'PHPMailer/src/Exception.php';
 // Generar PDF
 class PDF extends FPDF {
     function Header() {
-        \$this->Image('pdf.pdf', 0, 0, 210, 297); // Fondo PDF
         // Fondo visual
         $this->Image('pdf.pdf', 0, 0, 210, 297); // Tamaño A4
         $this->SetFont('Arial','B',14);
