3aeb9511f77213b68f5e8ab9968f4685650a1f90
formulario-it <extractofuec@gmail.com>
2025-04-15 12:42:58 -0500

    update

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php // index.php original del usuario con mejoras ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Formulario FUEC</title>
+  <link rel="stylesheet" href="css/estilos.css">
+  <script src="js/autocompletar.js" defer></script>
+</head>
+<body>
+  <h1>Formulario FUEC</h1>
+  <!-- Aquí iría tu formulario completo del FUEC -->
+</body>
+</html>
